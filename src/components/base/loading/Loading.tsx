afb9d6a08bc4e1bc0bceec68c415cde1d85921bd
xing <17621404964@163.com>
2024-03-05 15:45:23 +0800

    Loading组件开发

---
+++ src/components/base/loading/Loading.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react'
+import styles from './Loading.module.scss'
+import Image from './loading.gif'
+
+interface LoadingProps {
+  title?: string
+}
+
+const Loading: FC<LoadingProps> = ({ title = '正在加载...' }) => {
+  return (
+    <div className={styles['loading']}>
+      <div className={styles['loading-content']}>
+        <img width="24" height="24" src={Image} />
+        <p className={styles['desc']}>{title}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Loading
--- src/view/Recommend.tsx
+++ src/view/Recommend.tsx
@@ -1,58 +1,59 @@
-import { Suspense, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getRecommend } from '../service/recommend'
 import styles from './Recommend.module.scss'
 import Slider from '../components/base/slider/Slider'
 import Scroll from '../components/base/scroll/Scroll'
 import type { Albums, Sliders } from './type'
-import defaultImg from '../assets/images/default.png'
+import Loading from '../components/base/loading/Loading'
 
 function Recommend() {
   const [sliders, setSliders] = useState<Sliders>([])
   const [albums, setAlbums] = useState<Albums>([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const get = async () => {
       const result = await getRecommend()
       setAlbums(result.albums)
       setSliders(result.sliders)
+      setIsLoading(false)
     }
 
     get()
   }, [])
-
-  return (
-    <div className={styles.recommend}>
-      <Scroll cls={styles['recommend-content']} click>
-        <div>
-          <div className={styles['slider-wrapper']}>
-            <div className={styles['slider-content']}>
-              {sliders.length && <Slider sliders={sliders} />}
+  if (isLoading) {
+    return <Loading />
+  } else {
+    return (
+      <div className={styles.recommend}>
+        <Scroll cls={styles['recommend-content']} click>
+          <div>
+            <div className={styles['slider-wrapper']}>
+              <div className={styles['slider-content']}>
+                {sliders.length && <Slider sliders={sliders} />}
+              </div>
+            </div>
+            <div className={styles['recommend-list']}>
+              <h1 className={styles['list-title']}>热门歌单列表</h1>
+              <ul>
+                {albums.map((item) => (
+                  <li className={styles['item']} key={item.id}>
+                    <div className={styles['icon']}>
+                      <img src={item.pic} height="60" width="60" />
+                    </div>
+                    <div className={styles['text']}>
+                      <h2 className={styles['name']}>{item.username}</h2>
+                      <p className={styles['title']}>{item.title}</p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-          <div className={styles['recommend-list']}>
-            <h1 className={styles['list-title']}>热门歌单列表</h1>
-            <ul>
-              {albums.map((item) => (
-                <li className={styles['item']} key={item.id}>
-                  <div className={styles['icon']}>
-                    <Suspense
-                      fallback={<img src={defaultImg} height="60" width="60" />}
-                    >
-                      <img src={item.pic} height="60" width="60" />
-                    </Suspense>
-                  </div>
-                  <div className={styles['text']}>
-                    <h2 className={styles['name']}>{item.username}</h2>
-                    <p className={styles['title']}>{item.title}</p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </Scroll>
-    </div>
-  )
+        </Scroll>
+      </div>
+    )
+  }
 }
 
 export default Recommend
