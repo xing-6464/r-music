1c1b05a4f7f7e1e018e6529d9a23be149c782723
xing <17621404964@163.com>
2024-03-05 18:30:14 +0800

    歌手列表固定标题实现（中）

--- src/components/base/indexList/IndexList.tsx
+++ src/components/base/indexList/IndexList.tsx
@@ -9,25 +9,33 @@ interface IndexListProps {
   data: Singers
 }
 const IndexList: FC<IndexListProps> = ({ data }) => {
-  const { groupRef, onScroll } = useFixed(data)
-
+  const { groupRef, fixedTitle, onScroll } = useFixed(data)
   return (
     <Scroll cls={styles['index-list']} click probeType={3} emit={onScroll}>
-      <ul ref={groupRef}>
-        {data.map((group) => (
-          <li key={group.title} className={styles['group']}>
-            <h2 className={styles['title']}>{group.title}</h2>
-            <ul>
-              {group.list.map((item) => (
-                <li key={item.id} className={styles['item']}>
-                  <img className={styles['avatar']} src={item.pic} />
-                  <span className={styles['name']}>{item.name}</span>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+      <>
+        <ul ref={groupRef}>
+          {data.map((group) => (
+            <li key={group.title} className={styles['group']}>
+              <h2 className={styles['title']}>{group.title}</h2>
+              <ul>
+                {group.list.map((item) => (
+                  <li key={item.id} className={styles['item']}>
+                    <img className={styles['avatar']} src={item.pic} />
+                    <span className={styles['name']}>{item.name}</span>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+        {!fixedTitle ? (
+          <></>
+        ) : (
+          <div className={styles.fixed}>
+            <div className={styles['fixed-title']}>{fixedTitle}</div>
+          </div>
+        )}
+      </>
     </Scroll>
   )
 }
--- src/components/base/indexList/useFixed.ts
+++ src/components/base/indexList/useFixed.ts
@@ -1,32 +1,49 @@
-import { useLayoutEffect, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import { Singers } from '../../../view/type'
 
 export default function useFixed(data: Singers) {
   const groupRef = useRef<HTMLUListElement | null>(null)
-  const [, setListHeight] = useState<number[]>([])
-  const [, setScrollY] = useState<number>()
+  const [listHeight, setListHeight] = useState<number[]>([])
+  const [scrollY, setScrollY] = useState<number>()
+  const [currentIndex, setCurrentIndex] = useState(0)
 
-  useLayoutEffect(() => {
+  const fixedTitle = useMemo(() => {
+    if (scrollY && scrollY < 0) {
+      return ''
+    }
+    const currentGroup = data[currentIndex]
+    return currentGroup ? currentGroup.title : ''
+  }, [currentIndex, data, scrollY])
+
+  useEffect(() => {
     calculate()
   }, [data])
+
+  useEffect(() => {
+    for (let i = 0; i < listHeight.length - 1; i++) {
+      const heightTop = listHeight[i]
+      const heightBottom = listHeight[i + 1]
+      if (scrollY && scrollY >= heightTop && scrollY <= heightBottom) {
+        setCurrentIndex(i)
+      }
+    }
+  }, [scrollY])
 
   const calculate = () => {
     const list = groupRef.current?.children
     let height = 0
 
     setListHeight(() => [])
-    setListHeight((prev) => {
-      prev.push(height)
-      return prev
-    })
+    set(height)
 
     for (let i = 0; i < list!.length; i++) {
       height += list![i].clientHeight
-      setListHeight((prev) => {
-        prev.push(height)
-        return prev
-      })
+      set(height)
     }
+  }
+
+  function set(height: number) {
+    setListHeight((prev) => [...prev, height])
   }
 
   function onScroll(pos: any) {
@@ -35,6 +52,7 @@ export default function useFixed(data: Singers) {
 
   return {
     groupRef,
+    fixedTitle,
     onScroll,
   }
 }
