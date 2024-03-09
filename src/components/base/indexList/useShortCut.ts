fb0efcb49896d56736ad008b693657c75295daea
xing <17621404964@163.com>
2024-03-09 18:45:00 +0800

    歌手列表快速导航入口实现（03）

--- src/components/base/indexList/useShortCut.ts
+++ src/components/base/indexList/useShortCut.ts
@@ -1,10 +1,12 @@
 import { Singers } from '@/view/type'
-import React, { useMemo, useRef } from 'react'
+import React, { useMemo, useRef, useState } from 'react'
 
 export default function useShortCut(
   data: Singers,
   groupRef: React.MutableRefObject<HTMLUListElement | null>,
 ) {
+  const ANCHOR_HEIGHT = 18
+
   const scrollRef = useRef<any>()
 
   const shortcutList = useMemo(() => {
@@ -13,25 +15,52 @@ export default function useShortCut(
     })
   }, [data])
 
+  const [touch, setTouch] = useState<{
+    y1?: number
+    y2?: number
+    anchorIndex?: number
+  } | null>(null)
+
   function stopAndDefault(e: React.TouchEvent<HTMLDivElement>) {
     e.preventDefault()
     e.stopPropagation()
   }
 
   function onShortcutTouchStart(e: React.TouchEvent<HTMLDivElement>) {
-    // stopAndDefault(e)
+    stopAndDefault(e)
     const anchorIndex = parseInt(e.target.dataset.index)
-    const targetEl = groupRef.current?.children[anchorIndex]
-    const scroll = scrollRef.current.scroll.current
-    scroll.scrollToElement(targetEl, 50, false, false)
+    setTouch((prev) => ({
+      ...prev,
+      y1: e.touches[0].pageY,
+      anchorIndex: anchorIndex,
+    }))
+    console.log(touch)
+
+    scrollTo(anchorIndex)
   }
 
   function onShortcutTouchMove(e: React.TouchEvent<HTMLDivElement>) {
-    // stopAndDefault(e)
+    stopAndDefault(e)
+    setTouch((prev) => ({ ...prev, y2: e.touches[0].pageY }))
+    if (touch && touch.y2 && touch.y1 && touch.anchorIndex) {
+      const delta = ((touch.y2 - touch.y1) / ANCHOR_HEIGHT) | 0
+      const anchorIndex = touch.anchorIndex + delta
+      console.log(touch)
+      console.log(anchorIndex)
+
+      scrollTo(anchorIndex)
+    }
   }
 
   function onShortcutTouchEnd(e: React.TouchEvent<HTMLDivElement>) {
-    // stopAndDefault(e)
+    stopAndDefault(e)
+  }
+
+  function scrollTo(index: number) {
+    index = Math.max(0, Math.min(shortcutList.length - 1, index))
+    const targetEl = groupRef.current?.children[index]
+    const scroll = scrollRef.current.scroll.current
+    scroll.scrollToElement(targetEl, 20, false, false)
   }
 
   return {
