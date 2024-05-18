b62241463be7f37c10205385411424d12f71c046
xing-6464 <17621404964@163.com>
2024-05-19 00:54:35 +0800

    播放器播放按钮的暂停与播放逻辑开发

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -1,13 +1,24 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useMemo } from 'react'
 import { useAppSelector } from '@/store/hooks'
-import { currentSong as getCurrentSong } from '@/store/rootReducer'
+import {
+  currentSong as getCurrentSong,
+  setFullScreen,
+  setPlayingState,
+} from '@/store/rootReducer'
 import styles from './Player.module.scss'
+import { useAppDispatch } from '../../store/hooks'
+import classNames from 'classnames'
 
 function Player() {
   const audioRef = useRef<HTMLAudioElement>(null)
+  const dispatch = useAppDispatch()
   const fullScreen = useAppSelector((state) => state.root.fullScreen)
   const currentSong = useAppSelector(getCurrentSong)
-  console.log(currentSong)
+  const playing = useAppSelector((state) => state.root.playing)
+
+  const playIcon = useMemo(() => {
+    return playing ? '_icon-pause' : '_icon-play'
+  }, [playing])
 
   useEffect(() => {
     if (!currentSong.id || !currentSong.url) return
@@ -15,6 +26,23 @@ function Player() {
     audioRef.current.src = currentSong.url
     audioRef.current.play()
   }, [currentSong])
+
+  useEffect(() => {
+    if (!audioRef.current) return
+    playing ? audioRef.current.play() : audioRef.current.pause()
+  }, [playing])
+
+  function goBack() {
+    dispatch(setFullScreen(false))
+  }
+
+  function togglePlay() {
+    dispatch(setPlayingState(!playing))
+  }
+
+  function pause() {
+    dispatch(setPlayingState(false))
+  }
 
   return (
     <div className={styles.player}>
@@ -24,16 +52,37 @@ function Player() {
             <img src={currentSong.pic} />
           </div>
           <div className={styles.top}>
-            <div className={styles.back}>
+            <div className={styles.back} onClick={goBack}>
               <i className={styles['icon-back']}></i>
             </div>
             <h1 className={styles.title}>{currentSong.name}</h1>
-
             <h2 className={styles['subtitle']}>{currentSong.singer}</h2>
+          </div>
+          <div className={styles.bottom}>
+            <div className={styles.operators}>
+              <div className={classNames(styles.icon, styles['i-left'])}>
+                <i className="_icon-sequence"></i>
+              </div>
+              <div className={classNames(styles.icon, styles['i-left'])}>
+                <i className="_icon-prev"></i>
+              </div>
+              <div
+                className={classNames(styles.icon, styles['i-center'])}
+                onClick={togglePlay}
+              >
+                <i className={playIcon}></i>
+              </div>
+              <div className={classNames(styles.icon, styles['i-right'])}>
+                <i className="_icon-next"></i>
+              </div>
+              <div className={classNames(styles.icon, styles['i-right'])}>
+                <i className="_icon-not-favorite"></i>
+              </div>
+            </div>
           </div>
         </div>
       )}
-      <audio ref={audioRef}></audio>
+      <audio ref={audioRef} onPause={pause}></audio>
     </div>
   )
 }
